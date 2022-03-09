59e53185b0194406711bdd8cd36dd4338d9f5074
Singusek <anita0052@gmail.com>
2022-03-09 11:31:51 +0100

    first commit

---
+++ HumanPage/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+export default {
+    components: {
+        TheHeader
+    },
+}
+</script>
+
+<style>
+</style>
---
+++ HumanPage/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import ContactPage from '../pages/ContactPage.vue';
+import MainPage from '../pages/MainPage.vue';
+import PriceList from '../pages/PriceList.vue';
+import TimeTable from '../pages/TimeTable.vue';
+import NotFound from '../pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/main' },
+        { path: '/main', component: MainPage },
+        { path: '/contact', component: ContactPage },
+        { path: '/price-list', component: PriceList },
+        { path: '/timetable', component: TimeTable },
+        { path: '/:notFound(.*)', component: NotFound }
+    ]
+});
+
+export default router;
