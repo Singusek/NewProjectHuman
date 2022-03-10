d896a75c7ca31690678002bdf17e94ab44ddb902
Singusek <anita0052@gmail.com>
2022-03-10 14:05:34 +0100

    new componen in main page

---
+++ HumanPage/src/components/UI/Gallery.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <h4>Galeria</h4>
+        <div class="row">
+            <div class="col 14 m4 s12">
+                <img src="src/img/1-1.jpg" alt="" class="card responsive-img" />
+            </div>
+            <div class="col 14 m4 s12">
+                <img src="src/img/2-1.jpg" alt="" class="card responsive-img" />
+            </div>
+            <div class="col 14 m4 s12">
+                <img src="src/img/3-1.jpg" alt="" class="card responsive-img" />
+            </div>
+            <div class="col 14 m4 s12">
+                <img src="src/img/4.jpg" alt="" class="card responsive-img" />
+            </div>
+            <div class="col 14 m4 s12">
+                <img src="src/img/5.jpg" alt="" class="card responsive-img" />
+            </div>
+            <div class="col 14 m4 s12">
+                <img src="src/img/6.jpg" alt="" class="card responsive-img" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.container {
+    background-color: white;
+}
+h4 {
+    margin-left: auto;
+    margin-right: auto;
+    text-align:center;
+}
+</style>
--- HumanPage/src/pages/MainPage.vue
+++ HumanPage/src/pages/MainPage.vue
@@ -1,8 +1,37 @@
 <template>
 <div class="main-div">
     <the-header></the-header>
+    <div class="photo">
     <base-card></base-card>
+    </div>
     <main-card></main-card>
+    <div class="main-photo">
+    <div class="photo">
+        <div class="h5"> 
+        <h5 class="icons">Trenując z nami kształtujesz :</h5>
+        </div>
+        <a style="display: flex" class="op">
+            <div class="icons">
+            <img src="src/img/technika.png" style="margin: 20px; padding: 25px" />
+            <h6>Technika</h6>
+            </div>
+            <div>
+            <img src="src/img/biceps.png" style="margin: 20px; padding: 25px"/>
+            <h6 class="icons" style="padding-bottom: 25px;">Siła</h6>
+            </div>
+            <div>
+            <img src="src/img/mozg.png" style="margin: 20px; padding: 25px"/>
+            <h6 class="icons">Charakter</h6>
+            </div>
+            <div>
+            <img src="src/img/serce.png" style="margin: 20px; padding: 25px"/>
+            <h6 class="icons">Kondycja</h6>
+            </div>
+        </a>
+    </div>
+    <gallery></gallery>
+    </div>
+
 </div>
 </template>
 
@@ -10,16 +39,42 @@
 import TheHeader from '../components/layout/TheHeader.vue';
 import BaseCard from '../components/UI/BaseCard.vue';
 import MainCard from '../components/UI/MainCard.vue';
+import Gallery from '../components/UI/Gallery.vue';
 export default {
     components: {
         TheHeader,
         BaseCard,
-        MainCard
-        
+        MainCard,
+        Gallery
     },
 }
 </script>
+
+
 <style>
+.photo {
+    background-color: rgb(54, 53, 53);  
+    padding: 15px;
+    
+     
+}
+
+.icons {
+    color: white;
+    
+}
+.op {
+    justify-content: center;
+    padding: 10px;
+    text-align:center;
+}
+.h5 {
+    margin-left: auto;
+    margin-right: auto;
+    width: 23em;
+    padding: 15px;
+    text-align:center;
+}
 
    
 </style>
