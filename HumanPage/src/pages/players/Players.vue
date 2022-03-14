d73c8243ee0193d06eb42749c30830b757dddfe3
Singusek <anita0052@gmail.com>
2022-03-14 15:45:14 +0100

    #10 function fo reading data - working

--- HumanPage/src/pages/players/Players.vue
+++ HumanPage/src/pages/players/Players.vue
@@ -1,27 +1,19 @@
 <template>
     <section class="container">
         <main>
-            <h2>{{ name }}</h2>
+            <h2 v-for="name in name" :key="name.cadre">{{ name.name }}</h2>
         </main>
     </section>
 </template>
 
 <script>
-import { ref } from 'vue';
-
-import PLAYERS_DATA from '../players/players.json';
+import playersData from '../players/players.json';
 
 export default {
-    setup() {
-      const players = ref(PLAYERS_DATA);
-      const selectedPlayers = ref(null);
-
-      function selectPlayers(name) {
-          selectedPlayers.value = players.find((usr) => usr.name === name );
-      }
-
-      return { selectedPlayers, players, selectPlayers };
+        data() {
+            return {
+                name: playersData,
+        };
     },
-   
 }
 </script>
