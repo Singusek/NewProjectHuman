046e60d6a805ad26e9725aef3e28f8e88b40b0ef
Singusek <anita0052@gmail.com>
2022-03-22 12:19:05 +0100

    Issue_#11_components

---
+++ HumanPage/src/pages/trainers/TrainersPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <the-header></the-header>
+        <div class="container">
+            <div class="col s12 m8 offset-m2 l6 offset-l3">
+                <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s2">
+                    <img src="src/img/trainer.jpg" alt="" class="circle responsive-img img"> <!-- notice the "circle" class -->
+                    </div>
+                    <div class="col s10">
+                    <span class="black-text" v-for="name in name" :key="name.id">{{ name }}</span>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <the-footer></the-footer>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            trainer: [],
+        };
+    },
+    async created() {
+        try {
+            const res = await axions.get('trainer.json');
+            this.trainer = res.data;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
+</script>
+
+<style scoped>
+.img {
+    height: 200px;
+    width: 200px;
+    border:  5px rgb(202, 130, 21) solid;
+    
+}
+</style>
