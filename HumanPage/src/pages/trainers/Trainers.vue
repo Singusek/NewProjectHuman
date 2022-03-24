b7275c77cd6b14ac491304bfb4d69ba44c7d60f7
Singusek <anita0052@gmail.com>
2022-03-24 19:27:58 +0100

    Issue_#11_new_component

---
+++ HumanPage/src/pages/trainers/Trainers.vue
@@ -0,0 +1,137 @@
+<template>
+<div>
+    <div class="container">
+            <div class="col s12 m8 offset-m2 l6 offset-l3 ">
+                <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s2">
+                    <img src="src/img/trainer.jpg" alt="" class="circle responsive-img img"> 
+                    </div>
+                    <div class="col s10">
+                    <div v-for="trainer in trainers" :key="trainer.id"><h6 v-if="trainer.id == 1">{{ trainer.name }}</h6></div>
+                    <div v-for="trainer in trainers" :key="trainer.id"><p v-if="trainer.id == 1">{{ trainer.info }}</p></div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col s12 m8 offset-m2 l6 offset-l3">
+                <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s10">
+                    <div v-for="trainer in trainers" :key="trainer.id"><h6 v-if="trainer.id == 2">{{ trainer.name }}</h6></div>
+                    <div v-for="trainer in trainers" :key="trainer.id"><p v-if="trainer.id == 2">{{ trainer.info }}</p></div>
+                    </div>
+                    <div class="col s2">
+                    <img src="src/img/trainer.jpg" alt="" class="circle responsive-img img"> 
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col s12 m8 offset-m2 l6 offset-l3">
+                <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s2">
+                    <img src="src/img/trainer.jpg" alt="" class="circle responsive-img img"> 
+                    </div>
+                    <div class="col s10">
+                    <div v-for="trainer in trainers" :key="trainer.id"><h6 v-if="trainer.id == 3">{{ trainer.name }}</h6></div>
+                    <div v-for="trainer in trainers" :key="trainer.id"><p v-if="trainer.id == 3">{{ trainer.info }}</p></div>
+                    <p>Puchar Świata K-1 Rules Węgry, Szeged 2011 r.
+                        <br/> Zawodowe mistrzostwo Polski K-1 Zawodowy pas federacji USW-M, 2014
+                        <br/>Mistrz Polski Muay Thai, Świebodzice 2011
+                        <br/>Vice Mistrz Europy K-1 Rules, Włochy 2011
+                        <br/>Mistrz Polski Seniorów low-kick, 2014
+                        <br/> Międzynarodowy Mistrz Polski K-1 Rules, Gdańsk 2011
+                        <br/>Międzynarodowy Mistrz Litwy seniorów K-1 Rules 63.5kg, 2012
+                        <br/>Międzynarodowy Mistrz Litwy seniorów K-1 Rules 67kg, 2012
+                        <br/>Mistrz Polski Kick-Boxing K-1 Rules, Głogów 2010
+                        <br/>Mistrz Polski Kick-Boxing K-1 Rules, Głogów 2011
+                        <br/>Mistrz Polski w Kick-Boxingu K1 i low kick, Skarżysko Kamienna 2011
+                        <br/>Najlepszy zawodnik Mistrzostw Polski w Kick-Boxingu K1 i low-kick, Skarżysko Kamienna 2011
+                        <br/>Puchar Polski full contact, Węgrów 2010
+                    </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col s12 m8 offset-m2 l6 offset-l3">
+                <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s10">
+                    <div v-for="trainer in trainers" :key="trainer.id"><h6 v-if="trainer.id == 4">{{ trainer.name }}</h6></div>
+                    <div v-for="trainer in trainers" :key="trainer.id"><p v-if="trainer.id == 4">{{ trainer.info }}</p></div>
+                    </div>
+                    <div class="col s2">
+                    <img src="src/img/trainer.jpg" alt="" class="circle responsive-img img"> 
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col s12 m8 offset-m2 l6 offset-l3">
+                <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s2">
+                    <img src="src/img/trainer.jpg" alt="" class="circle responsive-img img"> 
+                    </div>
+                    <div class="col s10">
+                    <div v-for="trainer in trainers" :key="trainer.id"><h6 v-if="trainer.id == 5">{{ trainer.name }}</h6></div>
+                    <div v-for="trainer in trainers" :key="trainer.id"><p v-if="trainer.id == 5">{{ trainer.info }}</p></div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+</div>
+</template>
+
+<script>
+import trainer from './trainer.json';
+
+export default {
+    name: 'NameSingle',
+    data() {
+        return {
+            name: trainer,
+            singleName: undefined,
+            info: trainer,
+            trainers: trainer
+        }
+    },
+    computed: {
+        name () {
+            return this.$route.params.id
+        }
+    },
+    created() {
+        this.assingSingaleName();
+    },
+    methods: {
+        assingSingaleName() {
+            //  let data = this.name
+            //  console.log(data[1]);
+            //  let json
+
+            //  this.singleName = data.filter(t => t.id == 2);
+        }
+        
+    },
+}
+</script>
+
+
+<style scoped>
+.img {
+    height: 200px;
+    width: 200px;
+    border:  5px rgb(202, 130, 21) solid;
+    
+}
+</style>
--- HumanPage/src/pages/trainers/TrainersPage.vue
+++ HumanPage/src/pages/trainers/TrainersPage.vue
@@ -1,84 +1,20 @@
 <template>
     <div>
         <the-header></the-header>
-        <div class="container">
-            <div class="col s12 m8 offset-m2 l6 offset-l3">
-                <div class="card-panel grey lighten-5 z-depth-1">
-                <div class="row valign-wrapper">
-                    <div class="col s2">
-                    <img src="src/img/trainer.jpg" alt="" class="circle responsive-img img"> <!-- notice the "circle" class -->
-                    </div>
-                    <div class="col s10">
-                    <!-- <span class="black-text" v-for="name in name" :key="name">{{ name.name }}</span> -->
-                    <p>{{ singleName }}{{ singleName.id }}</p>
-                    <!-- <p v-for="name in assingSingaleName($route.params.id)" :key="name.id">{{ name }}</p> -->
-                    </div>
-                </div>
-                </div>
-            </div>
+        <div>
+        
+         <trainers></trainers>
         </div>
         <the-footer></the-footer>
     </div>
 </template>
 
 <script>
-// import axios from "axios";
-import trainer from '../trainers/trainer.json';
+import Trainers from './Trainers.vue';
 
 export default {
-    name: 'NameSingle',
-    data() {
-        return {
-            name: trainer,
-            singleName: undefined,
-            info: trainer
-        }
-    },
-    computed: {
-        name () {
-            return this.$route.params.id
-        }
-    },
-    created() {
-        this.assingSingaleName();
-    },
-    methods: {
-        assingSingaleName() {
-            let data = this.name
-            data.filter(item => {
-                return item.id == this.id
-            });
-
-            this.singleName = data
-        }
-        
-    },
+    components: {
+        Trainers
+    }
 }
-
-
-//     name: "TrainerInfo",
-//     data() {
-//         return {
-//             trainer: []
-//         };
-//     },
-//     async created() {
-//         try {
-//             const res = await axios.get(`http://localhost:3001/trainer`);
-
-//             this.trainer = res.data;
-//         } catch (e) {
-//             console.error(e);
-//         }
-//     },
-// };
 </script>
-
-<style scoped>
-.img {
-    height: 200px;
-    width: 200px;
-    border:  5px rgb(202, 130, 21) solid;
-    
-}
-</style>
