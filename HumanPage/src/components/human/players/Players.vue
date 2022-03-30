0e1ae45ca25c6df3c3cf40de71fa10f663bfebc6
Singusek <anita0052@gmail.com>
2022-03-30 15:41:26 +0200

    Issue_#10

--- HumanPage/src/components/human/players/Players.vue
+++ HumanPage/src/components/human/players/Players.vue
@@ -1,93 +1,52 @@
 <template>
 <div>
   <the-header></the-header>
-    <div class="container">
-        <div class="row">
-  <div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator img" src="src/img/pudzian.jpg">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4"><div v-for="name1 in name1" :key="name1.id">{{ name1.name1 }}</div><i class="material-icons right">more_vert</i></span>
-      <p><a class="black-text"><div v-for="cadre1 in cadre1" :key="cadre1.id">{{ cadre1.cadre1 }}</div></a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Osiągnięcia<i class="material-icons right">close</i></span>
-      <p v-for="achievements1 in achievements1" :key="achievements1.id">{{ achievements1.achievements1 }}</p>
-    </div>
+  <div>
+    <player 
+    v-for="player in PLAYERS_JSON" 
+    :key="player.id"
+    :id="player.id"
+    :name="player.name"
+    :cadre="player.cadre"
+    :achievments="player.achievments" />
   </div>
-  <div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator img" src="src/img/pudzian.jpg">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4"><div v-for="name1 in name1" :key="name1.id">{{ name1.name1 }}</div><i class="material-icons right">more_vert</i></span>
-      <p><a class="black-text"><div v-for="cadre1 in cadre1" :key="cadre1.id">{{ cadre1.cadre1 }}</div></a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Osiągnięcia<i class="material-icons right">close</i></span>
-      <p v-for="achievements1 in achievements1" :key="achievements1.id">{{ achievements1.achievements1 }}</p>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator img" src="src/img/pudzian.jpg">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4"><div v-for="name1 in name1" :key="name1.id">{{ name1.name1 }}</div><i class="material-icons right">more_vert</i></span>
-      <p><a class="black-text"><div v-for="cadre1 in cadre1" :key="cadre1.id">{{ cadre1.cadre1 }}</div></a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Osiągnięcia<i class="material-icons right">close</i></span>
-      <p v-for="achievements1 in achievements1" :key="achievements1.id">{{ achievements1.achievements1 }}</p>
-    </div>
-  </div>
-  
-        </div>
-
-        <main>
-            <h5 v-for="name1 in name1" :key="name1.id">{{ name1.name1 }}</h5>
-            <h6 v-for="cadre1 in cadre1" :key="cadre1.id">{{ cadre1.cadre1 }}</h6>
-            <!-- <div v-for="array in achievements1" :key="array">
-            <div v-for="(nameAchievement) in array" :key="nameAchievement">{{ nameAchievement }}</div>
-            </div> -->
-        </main>
-    </div>
-    <the-footer></the-footer>
+  <the-footer></the-footer>
 </div>
 </template>
 
 <script>
+import Player from '../player/Player.vue';
 import TheFooter from '../../layout/TheFooter.vue';
-import playersData from '../../../data/PLAYERS_JSON.json';
+import PLAYERS_JSON from '../../../data/PLAYERS_JSON.json';
 
 export default {
-  components: { TheFooter },
-    
-        data() {
-            return {
-                name1: playersData,
-                cadre1: playersData,
-                achievements1: playersData,
-        };
-    },
+  components: { 
+    TheFooter,
+    Player
+   },
+  data() {
+     return {
+         PLAYERS_JSON: PLAYERS_JSON
+      };
+  },
     
 }
 
-//const array = achievements1;
-//const keys = array.map(arr => arr.nameAchievement)
+
 </script>
 
 <style scoped>
- .row {
-     display: flex;
-    padding: 20px;
- }
-
- .card {
-     margin: 10px;
- }
- .img {
-     height: 300px;
- }
+.container {
+    margin-top: 50px;
+    margin-bottom: 70px;  
+    text-align: justify;
+    font-size: 18px;                             
+}
+.title {
+    margin-top: 50px;
+}
+.trainer-text {
+    margin-bottom: 40px;
+    margin-top: 100px;
+}
 </style>
