47729fc7baf4c905bd27ebc24b9ebefe08fa2846
Singusek <anita0052@gmail.com>
2022-04-27 18:01:41 +0200

    Issue_#26

--- HumanPage/src/components/human/trainers/Trainers.vue
+++ HumanPage/src/components/human/trainers/Trainers.vue
@@ -73,17 +73,40 @@ export default {
             gsap.to(el, {
                 // ScrollTrigger: el.dataset.index * "#animation",
                 // scrollTrigger: ".animation",
+                // scrollTrigger: {
+				//     trigger: el,
+                //     markers: true
+			    // },
                 opacity: 1,
                 x: 0,
                 duration: 2,
                 onComplete: done,
-                ScrollTrigger: {
-                    trigger: ".container",
-                    start: "top top"
-                }
+                // scrollTrigger: el
                 // ScrollTrigger.getById()
-            })  
+            });
         }
+
+            // Vue.directive('clipscroll', {
+            // inserted: function (el, binding) {
+            //     let f = function (evt) {
+            //     var hasRun = false
+            //     if (!hasRun && window.scrollY > binding.value.start) {
+            //         hasRun = true
+            //         TweenMax.to(el, 2, {
+            //         morphSVG: binding.value.toPath,
+            //         ease: Sine.easeIn
+            //         })
+            //     }
+            //     if (window.scrollY > binding.value.end) {
+            //         window.removeEventListener('scroll', f)
+            //     }
+            //     }
+            //     window.addEventListener('scroll', f)
+            // }
+            // })
+                //  window.addEventListener('scroll')
+
+        
             // gsap.timeline({
             //     scrollTrigger: {
             //         trigger: ".container",
